08623fa638d64dd435fae089bed80fdd592b5baf
jimmychang851129 <jimmychang851129@gmail.com>
2020-07-30 19:20:43 +0800

    basic aes encrypt decrypt function add

--- Config.py
+++ Config.py
@@ -4,3 +4,10 @@ Basedir = ""
 AESKeyLength = 128
 logfile = os.path.join(Basedir,"log.txt")
 hashchainLength = 100
+PORT = 5000
+
+jwtFormat = {
+	"username":"",
+	"hashchainVal":"",
+	"cntDay":0,
+}
