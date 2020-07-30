1d9799162bea1e873693ebf7735c2adeb97cd645
jimmychang851129 <jimmychang851129@gmail.com>
2020-07-30 19:20:43 +0800

    basic aes encrypt decrypt function add

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for
 import os
 from flask import request
+import utils
+import Config as cm
 
 app = Flask(__name__)
 
@@ -12,10 +14,9 @@ def index():
 
 @app.route('/HMIquery', methods=['POST'])
 def handleHMIRequest():
-    level = request.form.get('HMIquery')
-    print("level = ", level)
-
+	level = request.form.get('HMIquery')
+	key = "c"*16	# tmp
+	return utils.AESEncrypt(str(level),key)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(port=cm.PORT, debug=True)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import socket,os
 import Config as cm
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+import math,json
 
 #################################
 # aes with GCM mod of operation #
@@ -9,15 +10,21 @@ from Crypto.Hash import SHA256
 # key: length 16
 # IV: length 16
 def AESEncrypt(data,key):
+	result = {
+		"cipher":"",
+		"IV":0
+	}
 	try:
+		key = key.encode()
+		data = data.encode() + b'\0'*(math.ceil(len(data)/16)*16 - len(data))
 		nonceIV = os.urandom(16)
+		result['IV'] = nonceIV.hex()
 		obj = AES.new(key, AES.MODE_CBC, nonceIV)
-		ct = obj.encrypt(data)
+		result['cipher'] = obj.encrypt(data).hex()
 	except Exception as e:
 		print("[utils.py error]AESEncrypt error")
 		print(str(e))
-		ct,nonceIV = "",0
-	return ct,nonceIV
+	return json.dumps(result)
 
 def AESDecrypt(cipher,key,IV):
 	try:
@@ -36,3 +43,11 @@ def writelog(data):
 				fw.write(str(ele)+"\n")
 		else:
 			fw.write(data+"\n")
+
+def HashChainInit(key):
+	l = []
+	hashf = SHA256.new()
+	for i in range(cm.hashchainLength):
+		hashf.update(key.encode())
+		l.append(hashf.hexdigest())
+	return l
